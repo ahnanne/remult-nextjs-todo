30981f9bd3ac7b1564cef01be5ddb6dc967ec8df
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-06-01 01:29:39 +0900

    [A] Task 엔티티 생성

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,5 +1,8 @@
 import { remultExpress } from 'remult/remult-express';
+import { Task } from '../shared/Task';
 
-export const api = remultExpress( {
+export const api = remultExpress({
     bodyParser: false, // 파싱은 next.js가 해주기 때문에 false로 설정
-} );
+    entities: [Task]
+});
+// Remult를 이용하여 서버의 api 모듈에 Task 엔티티 등록
