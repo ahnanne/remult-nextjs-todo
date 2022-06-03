10154745343c842bac5fc3d1a6d7de9b65402244
안예인 <54733637+ahnanne@users.noreply.github.com>
2022-06-03 17:41:13 +0900

    [U] 페이징, 정렬, 필터링

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,21 @@ const taskRepo = remult.repo(Task);
 
 const Home: NextPage = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [hideCompleted, setHideCompleted] = useState(false);
 
   useEffect(() => {
-    const fetchTasks = async () => {
+    const fetchTasks = async (hideCompleted: boolean) => {
       try {
         // https://remult.dev/docs/ref_repository.html#find
-        const data = await taskRepo.find();
+        const data = await taskRepo.find( {
+          limit: 20, // 페이징
+          orderBy: { // 정렬
+            completed: 'asc'
+          },
+          where: { // 필터링
+            completed: hideCompleted ? false : undefined
+          }
+        });
         setTasks(data);
       } catch (e) {
         window.alert('목록을 불러오는 중 문제가 발생했습니다.');
@@ -20,19 +29,28 @@ const Home: NextPage = () => {
       }
     };
 
-    fetchTasks();
-  }, []);
+    fetchTasks(hideCompleted);
+  }, [hideCompleted]);
 
   console.log(tasks);
 
   return (
     <div>
-      {tasks.map(({ id, title, completed }) =>
-        <div key={id}>
-          <input id={id} type="checkbox" checked={completed} />
-          <label htmlFor={id}>{title}</label>
-        </div>
-      )}
+      <input
+        id='hide-completed'
+        type='checkbox'
+        checked={hideCompleted}
+        onChange={() => setHideCompleted(!hideCompleted)}
+      />
+      <label htmlFor='hide-completed'>완료된 항목 숨기기</label>
+      <ul>
+        {tasks.map(({ id, title, completed }) =>
+          <li key={id}>
+            <input id={id} type="checkbox" checked={completed} />
+            <label htmlFor={id}>{title}</label>
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
