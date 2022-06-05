1d95c062d41080ff6ce77559c073e2267c96a4d1
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-06-06 01:42:52 +0900

    [U] validation 추가

--- src/shared/Task.ts
+++ src/shared/Task.ts
@@ -1,4 +1,4 @@
-import { Entity, Fields } from 'remult';
+import { Entity, Fields, Validators } from 'remult';
 
 // Entity 데코레이터는 Remult에게 이 클래스가 엔티티 클래스임을 알려준다.
 // API 라우트와 디폴트 db 콜렉션/테이블 이름으로 사용될 값을 첫 번째 매개변수(key)로 전달한다.
@@ -10,7 +10,14 @@ export class Task {
   @Fields.uuid()
     id!: string; // 옵셔널
 
-  @Fields.string()
+  @Fields.string({
+    // Remult는 이 validate 함수를 서버사이드에서 호출하여 유효성을 검증함.
+    validate: Validators.required
+    // validate: (task) => {
+    //   if (task.title.length < 3)
+    //     throw "Too Short";
+    // }
+  })
     title = '';
 
   @Fields.boolean()
