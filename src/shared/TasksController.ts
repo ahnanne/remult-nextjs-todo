318c6d02cd5c2d0c26010fdd5f7ef01ff0e44ce5
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-07-03 17:11:45 +0900

    [A] 백엔드 메서드 추가
    - `setAll` 함수를 프론트 단에서 돌릴 경우, PUT 요청이 개별적으로
      들어가서, 태스크 항목이 많아지면 성능 문제로 이어지게 된다.
    
    - Remult에선 그러한 성능 문제를 방지하기 위해, 동일한 동작을 프론트가
      아닌 서버에서 실행시킬 수 있는 기능을 지원한다kyle dion.

---
+++ src/shared/TasksController.ts
@@ -0,0 +1,16 @@
+import { BackendMethod, Remult } from "remult";
+import { Task } from "./Task";
+
+export class TasksController {
+  @BackendMethod({ allowed: true })
+  static async setAll(completed: boolean, remult?: Remult) {
+    // 이때 두번째 매개변수 remult는, 프론트 단에서 호출되는 시점에는 생략됨.
+    // @BackendMethod 데코레이터로 수식한 함수에는
+    // 서버사이드에서 서버의 Remult 객체가 주입됨.
+    const taskRepo = remult!.repo(Task);
+
+    for (const task of await taskRepo.find()) {
+      await taskRepo.save({ ...task, completed });
+    }
+  }
+}
