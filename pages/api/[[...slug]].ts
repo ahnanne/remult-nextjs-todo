5f68ecfc476d6399bdb9fa6aae63604d9fe22edf
안예인 <54733637+ahnanne@users.noreply.github.com>
2022-07-21 23:47:45 +0900

    [U] 미들웨어(express-jwt) 추가

--- pages/api/[[...slug]].ts
+++ pages/api/[[...slug]].ts
@@ -2,9 +2,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import * as util from 'util'; // https://github.com/nodejs/node/blob/v17.0.0/lib/util.js
 import { api } from '../../src/server/api';
+import { expressjwt } from 'express-jwt';
 
-const handler = async ( req: NextApiRequest, res: NextApiResponse ) => {
-    await util.promisify( ( api as any ) )( req, res );
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await util.promisify(expressjwt({
+    secret: process.env[ 'JWT_SECRET' ] || "my secret",
+    credentialsRequired: false,
+    algorithms: ['HS256'] // jsonwebtoken에서 사용되는 디폴트 알고리즘
+  }) as any)(req, res);
+  await util.promisify((api as any))(req, res);
 };
 
 export default handler;
